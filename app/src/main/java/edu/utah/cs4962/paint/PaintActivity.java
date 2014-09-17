f33d5b48ddcf869e6da784058f7d30e3cd80013a
Junior Matelau <matelau@gmail.com>
2014-09-17 09:32:04 -0600

    PaletteView drawing cirles

--- app/src/main/java/edu/utah/cs4962/paint/PaintActivity.java
+++ app/src/main/java/edu/utah/cs4962/paint/PaintActivity.java
@@ -14,52 +14,58 @@ import android.widget.LinearLayout;
 public class PaintActivity extends Activity {
     PaintView _paintView;
     PaintAreaView _paintAreaView;
-    PaletteView _paleteView;
+    PaletteView _paletteView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
-        LinearLayout rootLayout = new LinearLayout(this);
-        rootLayout.setWeightSum(1f);
+        PaletteView rootLayout = new PaletteView(this);
+//        rootLayout.setWeightSum(1f);
 
         Display display = ((WindowManager) getBaseContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
         int rotation = display.getRotation();
-        _paintAreaView = new PaintAreaView(this);
-        _paleteView = new PaletteView(this);
+//        _paintAreaView = new PaintAreaView(this);
+//        _paletteView = new PaletteView(this);
 
-
-        _paintView = new PaintView(this);
-        _paintView.setPadding(30, 30, 30, 30);
-        _paintView.setColor(Color.RED);
-        _paintView.setBackgroundColor(Color.BLACK);
-        _paintView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                _paintView.setColor(Color.GREEN);
-            }
-        });
-
-        if(rotation  == 0 ){
-            rootLayout.setOrientation(LinearLayout.VERTICAL);
-            rootLayout.addView(_paintAreaView, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, .5f));
-            rootLayout.addView(_paleteView, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, .5f));
-        }
-        else{
-            rootLayout.setOrientation(LinearLayout.HORIZONTAL);
-            rootLayout.addView(_paintAreaView, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, .5f));
-            rootLayout.addView(_paleteView, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, .5f));
+        for ( int splotchIndex = 0; splotchIndex <= 10; splotchIndex++)
+        {
+            PaintView paintView = new PaintView(this);
+            paintView.setColor(Color.RED);
+            paintView.setBackgroundColor(Color.BLACK);
+            paintView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//                    paintView.setColor(Color.GREEN);
+                    v.invalidate();
+                    v.setVisibility(View.GONE);
+                }
+            });
+            rootLayout.addView(paintView, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
 
         }
 
 
-        _paintView.setOnSplotchTouchListener(new PaintView.OnSplotchTouchListener() {
-            @Override
-            public void onSplotchTouched(PaintView v) {
-                _paintView.setColor(Color.BLUE);
-            }
-    });
+//        if(rotation  == 0 ){
+//            rootLayout.setOrientation(LinearLayout.VERTICAL);
+//            rootLayout.addView(_paintAreaView, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, .5f));
+//            rootLayout.addView(_paintView, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, .5f));
+//        }
+//        else{
+//            rootLayout.setOrientation(LinearLayout.HORIZONTAL);
+//            rootLayout.addView(_paintAreaView, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, .5f));
+//            rootLayout.addView(_paintView, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, .5f));
+//
+//        }
+
+
+//        _paintView.setOnSplotchTouchListener(new PaintView.OnSplotchTouchListener() {
+//            @Override
+//            public void onSplotchTouched(PaintView v) {
+//                _paintView.setColor(Color.BLUE);
+//            }
+//    });
 
 
 
@@ -70,7 +76,8 @@ public class PaintActivity extends Activity {
     @Override
     protected void onResume(){
         super.onResume();
-        _paintView.setColor(Color.BLUE);
+//        _paintView.setColor(Color.BLUE);
+
 
     }
 
--- app/src/main/java/edu/utah/cs4962/paint/PaletteView.java
+++ app/src/main/java/edu/utah/cs4962/paint/PaletteView.java
@@ -1,14 +1,12 @@
 package edu.utah.cs4962.paint;
 
 import android.content.Context;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PointF;
+import android.graphics.Rect;
 import android.graphics.RectF;
+import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.ArrayList;
 
 /**
  * A subclass of ViewGroup that overrides onDraw to draw a painter’s palette
@@ -26,97 +24,104 @@ import java.util.ArrayList;
  */
 public class PaletteView extends ViewGroup {
     RectF _contentRect;
-    ArrayList<Integer> paints;
 
     //TODO: Look into Application.class to save state.
 
     public PaletteView(Context context){
         super(context);
-        paints = new ArrayList<Integer>();
-        paints.add(Color.RED);
-        paints.add(Color.BLUE);
-        paints.add(Color.GREEN);
     }
 
-    @Override
-    protected void onDraw(Canvas canvas){
-        //TODO: draw palette add paints
-        _contentRect = new RectF(
-                getPaddingLeft(),
-                getPaddingTop(),
-                (float) getWidth() - getPaddingRight(),
-                (float) getHeight() - getPaddingBottom());
-
-        Paint palPaint = new Paint();
-        palPaint.setColor(0xff8b4513); // brown
-        palPaint.setStyle(Paint.Style.FILL_AND_STROKE);
-        float _radius = Math.min(_contentRect.width() * 0.5f, _contentRect.height() * 0.5f);
-        PointF center = new PointF(_contentRect.centerX(), _contentRect.centerY());
-        canvas.drawCircle(center.x, center.y, _radius, palPaint);
-
-    }
+//    @Override
+//    protected void onDraw(Canvas canvas){
+//        //TODO: draw palette add paints
+//        _contentRect = new RectF(
+//                getPaddingLeft(),
+//                getPaddingTop(),
+//                (float) getWidth() - getPaddingRight(),
+//                (float) getHeight() - getPaddingBottom());
+//
+//        Paint palPaint = new Paint();
+//        palPaint.setColor(0xff8b4513); // brown
+//        palPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+//        float _radius = Math.min(_contentRect.width() * 0.5f, _contentRect.height() * 0.5f);
+//        PointF center = new PointF(_contentRect.centerX(), _contentRect.centerY());
+//        canvas.drawCircle(center.x, center.y, _radius, palPaint);
+//
+//    }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
         //TODO: implement iterate through all the children then report how big you need to be
         // getChildAt() call measure, getChildCount()
 
-        final int cCount = getChildCount();
-
-        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-        // what I think width/height should be
         int widthSpec = MeasureSpec.getSize(widthMeasureSpec);
         int heightSpec = MeasureSpec.getSize(heightMeasureSpec);
 
-        //start small and try to grow
-        int width = getSuggestedMinimumWidth();
-        int height = getSuggestedMinimumHeight();
 
-        if(widthMode == MeasureSpec.AT_MOST)
-            width = widthSpec;
-        if(heightMode == MeasureSpec.AT_MOST)
-            height = heightSpec;
-
-        if( widthMode == MeasureSpec.EXACTLY)
-        {
-            width = widthSpec;
-            height = width;
+        for(int childIndex = 0; childIndex < getChildCount(); childIndex++){
+           View child = getChildAt(childIndex);
+            // pass in at most to give the pieces different spacing
+           int childWidthMeasureSpec = 50 | MeasureSpec.EXACTLY;
+           int childHeightMeasureSpec = 50 | MeasureSpec.EXACTLY;
+           child.measure(childWidthMeasureSpec, childHeightMeasureSpec);
         }
 
-        if( heightMode == MeasureSpec.EXACTLY)
-        {
-            height = heightSpec;
-            width = height;
-        }
-
-        //enforce squareness
-        if(width > height && widthMode != MeasureSpec.EXACTLY)
-            width = height;
-        if(height > width && heightMode != MeasureSpec.EXACTLY)
-            height = heightSpec;
+        int width = Math.max(widthSpec, getSuggestedMinimumWidth());
+        int height = Math.max(heightSpec,getSuggestedMinimumHeight() );
 
         // TODO: Respect padding
         setMeasuredDimension(
-                resolveSizeAndState(width, widthMeasureSpec, width < getSuggestedMinimumWidth() ? MEASURED_STATE_TOO_SMALL : 0),
-                resolveSizeAndState(height, heightMeasureSpec,  height < getSuggestedMinimumHeight() ? MEASURED_STATE_TOO_SMALL : 0));
-
+                resolveSizeAndState(width, widthMeasureSpec, 0),
+                resolveSizeAndState(height, heightMeasureSpec,  0));
     }
 
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-//        final int children = getChildCount(), height = getMeasuredHeight();
-//        for (int i = 0; i < children; i++) {
-//            final View view = getChildAt(i);
-//
-//            if (view.getVisibility() != View.VISIBLE)
-//                continue;
-//
-//            final int width = view.getMeasuredWidth();
-//            view.layout(l, 0, l + width, height);
-//            l += width;
-//        }
+        //getLeft() == l
+        Log.i("layout", "l:" + l + " getLeft(): " + getLeft());
+
+        // see what measure the children came up with
+        int childWidthMax = 0;
+        int childHeightMax = 0;
+        for(int childIndex = 0; childIndex < getChildCount(); childIndex++){
+            View child = getChildAt(childIndex);
+            childWidthMax= Math.max(childWidthMax, child.getMeasuredWidth());
+            childHeightMax= Math.max(childHeightMax, child.getMeasuredWidth());
+            //check for removed children
+        }
+
+        childWidthMax = childWidthMax/2;
+        childHeightMax = childHeightMax/2;
+
+        //encompassing rect
+        Rect layoutRect = new Rect();
+        layoutRect.left = getPaddingLeft() + childWidthMax;// lefthand side + move half the width of the child
+        layoutRect.top = getPaddingTop() + childWidthMax;
+        layoutRect.right = getWidth() - getPaddingRight() - childHeightMax;
+        layoutRect.bottom = getHeight() - getPaddingBottom() - childHeightMax;
+
+        for(int childIndex = 0; childIndex < getChildCount(); childIndex++)
+        {
+            double angle = (double) childIndex / (double)getChildCount() * 2.0 * Math.PI;
+            int childCenterX = (int) (layoutRect.centerX() + layoutRect.width() *  Math.cos(angle) * 0.5);
+            int childCenterY = (int) (layoutRect.centerY() + layoutRect.height() *  Math.sin(angle) * 0.5);
+
+
+            View child = getChildAt(childIndex);
+            if(child.getVisibility() == GONE)
+                child.layout(0,0,0,0);
+            else{
+                Rect childLayout = new Rect();
+                //coords in parents system
+                childLayout.left = childCenterX - childWidthMax ;
+                childLayout.right = childCenterX + childWidthMax ;
+                childLayout.top = childCenterY - childHeightMax ;
+                childLayout.bottom = childCenterY + childHeightMax ;
+                child.layout(childLayout.left, childLayout.top, childLayout.right, childLayout.bottom);
+            }
+
+        }
 
     }
 
